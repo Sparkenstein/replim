ae448489d9c6bb97e75eb66303f97515925a2d7d
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-15 15:32:30 +0530

    [unstable] trying out console

--- index.js
+++ index.js
@@ -3,20 +3,24 @@
 const term = require('terminal-kit').terminal;
 const { NodeVM } = require('vm2');
 const userInput = require('./lib/userInput');
+const stream = require('stream');
 
 (async function () {
+    const vmConsole = new console.Console({ stdout: new stream.PassThrough(), stderr: new stream.PassThrough() });
+
     const vm = new NodeVM({
         require: {
             external: true
         },
         console: 'inherit',
         sandbox: {
-            term: term
+            term: term,
+            vmConsole
         }
     });
 
     for await (const line of userInput(term)) {
-        vm.run(`\n${line}\n`);
+        vm.run(`\nvmConsole("hello")\n`);
         // term.green(`\n${output}\n`);
     }
     process.exit();
