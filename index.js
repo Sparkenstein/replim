b990044c39954534e1a0e2a69cd8aede6bc7aa9c
Sparkenstein <zetabytes.pp@gmail.com>
2019-03-15 12:37:54 +0530

    moved generator function to a file

--- index.js
+++ index.js
@@ -2,39 +2,12 @@
 
 const term = require('terminal-kit').terminal;
 const { VM } = require('vm2');
+const userInput = require('./lib/userInput');
 
 (async function () {
-    async function* userInput() {
-        while (true) {
-            term('js> ');
-            const ip = await term.inputField(
-                {
-                    history,
-                    autoComplete,
-                    autoCompleteMenu: false,
-                    autoCompleteHint: true
-                }
-            ).promise;
-            if (!ip) {
-                return;
-            }
-            yield ip;
-        }
-    }
-
     const vm = new VM();
 
-    var history = [];
-
-    var autoComplete = [
-        'const',
-        'let',
-        'var',
-        'function',
-        'console'
-    ];
-
-    for await (const line of userInput()) {
+    for await (const line of userInput(term)) {
         const output = vm.run(`${line}`);
         term.green(`\n${output}\n`);
     }
